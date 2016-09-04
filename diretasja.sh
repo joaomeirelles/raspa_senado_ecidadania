d3530211a53b3c6cd8cd7008442dca6c0e2a4dff
joaomeirelles <joaovitormeirelles@gmail.com>
2016-09-04 16:44:55 +0200

    Add files via upload

--- diretasja.sh
+++ diretasja.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-Rscript diretasja.R
+#!/bin/bash -e
+
+#python scrape_votacao_novaseleicoes.py
+
+
+for run in {1..10}
+do
+	echo "msg3"
+	#atualiza a cada 10 min
+	Rscript diretasja.R
+
+	git add . 
+	git commit -m "atualizacao"
+	git push https://github.com/joaomeirelles/raspa_senado_ecidadania
+	sleep 1m
+done
